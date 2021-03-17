8f6f8e81dab9773e8a8d8bf06ac8dd80c3f8232b
royarkaprava <34636927+royarkaprava@users.noreply.github.com>
2021-03-17 19:01:51 -0400

    data edits

--- Usage.R
+++ Usage.R
@@ -12,7 +12,7 @@ library(OPMpackage)
 ##################Simulated data##################
 ##################################################
 
-load("~/GitHub/OPM/data.rda")
+load("~/GitHub/OPM/Simulated data/Mm_50_rep_1.rda")
 phenotypes2 <- Macro.micro_2_test
 data <- phenotypes2
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
@@ -26,7 +26,7 @@ out <- fit.ALASSOOPM(Ymat[, 1], X, LAMBDA = 0.5, ncores = 1, test, train, IndMa)
 ##################################################
 ##################Realdata data##################
 ##################################################
-load("~/GitHub/OPM/Real data/Macro.micro_1.rda")
+load("~/GitHub/OPM/Real data/Macro.micro_final_1.rda")
 data <- Macro.micro
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
 X    <- data[, -c(1:6)] #The design matrix with first 20 columns are macrovariables and rest are microvariables
@@ -49,7 +49,7 @@ out <- fit.ALASSOOPM(Ymat[, 1], X, LAMBDA = 0.5, ncores = 1, test, train, IndMa)
 ##################Simulated data##################
 ##################################################
 
-load("data.rda")
+load("~/GitHub/OPM/Simulated data/Mm_50_rep_1.rda")
 phenotypes2 <- Macro.micro_2_test
 data <- phenotypes2
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
@@ -63,7 +63,7 @@ out <- fit.SCADOPM(Ymat[, 1], X, LAMBDA = 0.5, 1, test, train, IndMa)
 ##################################################
 ##################Realdata data##################
 ##################################################
-load("~/GitHub/OPM/Real data/Macro.micro_1.rda")
+load("~/GitHub/OPM/Real data/Macro.micro_final_1.rda")
 data <- Macro.micro
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
 X    <- data[, -c(1:6)] #The design matrix with first 20 columns are macrovariables and rest are microvariables
@@ -89,7 +89,7 @@ out <- fit.SCADOPM(Ymat[, 1], X, LAMBDA = 0.5, ncores = 1, test, train, IndMa)
 ##################################################
 
 
-load("data.rda")
+load("~/GitHub/OPM/Simulated data/Mm_50_rep_1.rda")
 phenotypes2 <- Macro.micro_2_test
 data <- phenotypes2
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
@@ -104,7 +104,7 @@ out <- fit.RAWOPM(Ymat[, 1], X, LAMBDA = 0.5, 1, test, train, IndMa)
 ##################################################
 ##################Realdata data##################
 ##################################################
-load("~/GitHub/OPM/Real data/Macro.micro_1.rda")
+load("~/GitHub/OPM/Real data/Macro.micro_final_1.rda")
 data <- Macro.micro
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
 X    <- data[, -c(1:6)] #The design matrix with first 20 columns are macrovariables and rest are microvariables
@@ -132,7 +132,7 @@ out <- fit.RAWOPM(Ymat[, 1], X, LAMBDA = 0.5, ncores = 1, test, train, IndMa)
 ##################################################
 
 
-load("data.rda")
+load("~/GitHub/OPM/Simulated data/Mm_50_rep_1.rda")
 phenotypes2 <- Macro.micro_2_test
 data <- phenotypes2
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
@@ -146,7 +146,7 @@ out <- fit.AENOPM(Ymat[, 1], X, LAMBDA = 0.5, 1, test, train, IndMa)
 ##################################################
 ##################Realdata data##################
 ##################################################
-load("~/GitHub/OPM/Real data/Macro.micro_1.rda")
+load("~/GitHub/OPM/Real data/Macro.micro_final_1.rda")
 data <- Macro.micro
 Ymat <- data[, 2:6] #The 5 different class label vectors with different proportions of 1
 X    <- data[, -c(1:6)] #The design matrix with first 20 columns are macrovariables and rest are microvariables
